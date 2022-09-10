218646b04065c116e66dc1c9fadee2f86dfc6ece
Dhrubajyoti Das <dhrubajyoti1212@gmail.com>
2022-09-10 12:34:14 +0600

    Add files via upload

---
+++ stored_procedure_example/newdb.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Sep 10, 2022 at 06:33 AM
+-- Server version: 8.0.27
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `newdb`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+DROP PROCEDURE IF EXISTS `insert_val`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_val` (`fname` VARCHAR(30), `lname` VARCHAR(30), `contacts` INT, `address_c` VARCHAR(50))  BEGIN
+insert into users(first_name,last_name,contact_no,address) VALUES(fname,lname,contacts,address_c);
+END$$
+
+DROP PROCEDURE IF EXISTS `read_data`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `read_data` ()  BEGIN
+SELECT * from users;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `contact_no` int NOT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `contact_no`, `address`) VALUES
+(1, 'abc', 'd', 123456, 'ctg'),
+(2, 'def', 'cd', 567678, 'dhaka');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
